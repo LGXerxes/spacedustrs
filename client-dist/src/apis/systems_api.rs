60feb8a4fa1ef0d0cbb26bcbc2496e700f86963e
LGXerxes <ditisluca@yahoo.it>
2023-12-01 00:19:29 +0100

    test

--- client-dist/src/apis/systems_api.rs
+++ client-dist/src/apis/systems_api.rs
@@ -11,7 +11,7 @@
 
 use reqwest;
 
-use crate::apis::ResponseContent;
+use crate::{apis::ResponseContent, models::{WaypointType, GetSystemWaypointsTraitsParameter}};
 use super::{Error, configuration};
 
 
